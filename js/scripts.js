c9660683a3c7abcde00f71c0de4d9f73e59e8627
Duncan Robbins <duncanrobbins19@gmail.com>
2019-08-16 13:54:53 -0700

    fix the way the output is displayed and allows it to be done correctly

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,15 @@ $(document).ready(function() {
     var pet =  parseInt($("#pet").val());
     var eye = parseInt($("#eye").val());
     var day =  parseInt($("#day").val())
-if (area + color + pet + eye + day === 10) {
 
-        $(".results").show();
-}
-// if (area + color + pet + eye + day > 11{
-//   $(".results2").show();
-// }
-// if (area + color + pet + eye + day < 9){
-//   $(".results3").show();
-// }
-
+    if (area + color + pet + eye + day === 10) {
+      $("#results").show();
+    } else if (area + color + pet + eye + day > 10){
+      $("#results2").show();
+    } else {
+      $("#results3").show();
+    }
+// if (area + color + pet + eye + day < 10)
     event.preventDefault();
   });
 });
